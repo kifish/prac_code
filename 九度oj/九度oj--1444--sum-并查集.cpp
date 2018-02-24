2c2a7d179c9728228c05ad0eeb0b2a7dacc18f0a
kifish <huakaihuakai@qq.com>
2018-02-25 00:15:21 +0800

    Create 九度oj--1444--sum-并查集.cpp

---
+++ 九度oj/九度oj--1444--sum-并查集.cpp
@@ -0,0 +1,46 @@
+//合并集合的时候要注意。
+
+
+#include<cstdio>
+
+#define N 10000005
+int root[N];
+int findroot( int x){
+    if(root[x]==-1) return x;
+    else{
+        int tmp=findroot(root[x]);
+        root[x]=tmp;
+        return tmp;
+    }
+}
+
+int sum[N];
+int main(){
+    int n;
+    while(scanf("%d",&n)!=EOF){
+        for(int i=1;i<=10000000;i++){
+            root[i]=-1;
+            sum[i]=1;
+        }
+        while(n--){
+            int a,b;
+            scanf("%d %d",&a,&b);
+            a=findroot(a);
+            b=findroot(b);
+            if(a!=b){
+                //a=root[b]; mistake,a may be -1
+                root[b]=a;
+                sum[a]+=sum[b];
+            }
+        }
+        int max=1;
+        for(int i=1;i<=10000000;i++){
+            if(root[i]==-1&&sum[i]>max){
+                max=sum[i];
+                
+            }
+        }
+        printf("%d\n",max);
+    }
+    return 0;
+}
