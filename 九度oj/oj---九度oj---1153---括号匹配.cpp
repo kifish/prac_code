868b79fcf6f3f9941eb9f80e978afd8360aab63f
kifish <huakaihuakai@qq.com>
2018-02-25 00:10:33 +0800

    Create oj---九度oj---1153---括号匹配.cpp

---
+++ 九度oj/oj---九度oj---1153---括号匹配.cpp
@@ -0,0 +1,34 @@
+#include<cstdio>
+#include<stack>
+
+using namespace std;
+stack<int > S;
+char str[110];
+char res[110];
+int main(){
+    while(scanf("%s",str)!=EOF){
+        int i;
+        for(i=0;str[i]!=0;i++){
+            if(str[i]=='('){
+                S.push(i);
+                res[i]=' ';
+            }
+            else if(str[i]==')'){
+                if(!S.empty()){
+                    S.pop();
+                    res[i]=' ';
+                }
+                else res[i]='?';
+            }
+            else res[i]=' ';
+        }
+        while(!S.empty()){
+            res[S.top()]='$';
+            S.pop();
+        }
+        res[i]=0;
+        puts(str);
+        puts(res);
+    }
+    return 0;
+}
