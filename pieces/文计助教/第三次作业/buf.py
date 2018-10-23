e615b2d0c44b4fd77928db9233c031a2672872ae
kifish <huakaihuakai@qq.com>
2018-10-23 21:42:25 +0800

    -

--- pieces/文计助教/第三次作业/buf.py
+++ pieces/文计助教/第三次作业/buf.py
@@ -1,17 +1,74 @@
-rfile=open("/Users/k/Documents/prac_code/pieces/文计助教/第三次作业/20181017作业/50W.txt","r",encoding="utf-8")#打开50W.text文件，r代表动作，encoding代表输出格式
-fileContent=rfile.read()
-rfile.close()
+# coding = utf-8
+import re
 
-listA=fileContent.split()
-chnCount=len(listA)
-dictXing={}
-for i in range(0,chnCount):
-    tmp=listA[i]
-    xing=tmp[0]
-    if xing in dictXing:
-        dictXing[xing]+=1
+def findRepeatNum(paramStr):
+    result_list=[]
+    li = re.findall(r'[0-9]+', paramStr)
+    arr = [0]*20000
+    for item in li:
+        if arr[int(item)] ==0:
+             arr[int(item)]=1
+        else:
+            result_list.append(int(item))
+    return result_list
+
+def findRepeatNumAndCha(paramList, paramStr):
+    result_dic={}
+    for item in paramList:
+        li = re.findall(r"[\u4e00-\u9fa5]+\,"+str(item), paramStr)
+        for _item in li:
+            _li = _item.split(",")
+            result_dic[_li[0]] = int(_li[1])
+    return result_dic
+
+def replaceStr(paramList, paramStr):
+    result_str = paramStr
+    for item in paramList:
+        result_str = re.sub(r"[\u4e00-\u9fa5]+\,"+str(item), "", result_str)
+    return result_str
+
+def processStr(paramStr):
+    paramStr = paramStr.strip()
+    repeat_li = findRepeatNum(paramStr)
+    result_dic = findRepeatNumAndCha(repeat_li, paramStr)
+    result_str = replaceStr(repeat_li, paramStr)
+    return result_dic, result_str
+
+def judge(paramStr1, paramStr2):
+    dic1, str1 = processStr(paramStr1)
+    dic2, str2 = processStr(paramStr2)
+    # print(dic1,dic2)
+    # print(str1)
+    # print(str2)
+    # print(str2[0:2])
+    # print(dic1==dic2)
+    # print(str1==str2)
+
+    if dic1==dic2 and str1==str2:
+        return True
     else:
-        dictXing[xing]=1
-listB=list(dictXing.items())
-listB.sort(key=lambda x:x[1],reverse=True)
-#lambda是一个匿名函数，实际效果就是def一个函数
+        return False
+
+f1 = open("/Users/k/Documents/prac_code/pieces/文计助教/第三次作业/judge3/result.txtt", "r", encoding="utf-8")
+# f2 = open("anyoneelse.txt", "r")
+f2 = open("anyoneelse.txt", "r", encoding="utf-8")
+
+list1 = f1.readlines()
+list2 = f2.readlines()
+
+print(list2[0])
+
+result = True
+result_list = [True]*5
+for i in range(len(list1)):
+    if not judge(list1[i], list2[i]):
+        result= False
+        result_list[i]=False
+
+print(result_list)
+
+if result:
+    print(True)
+else:
+    print(False)
+
