f25202eb11dbbc052ece66e08571a106c818559c
kifish <huakaihuakai@qq.com>
2018-11-22 15:53:00 +0800

    -

--- pieces/文计助教/assignment5/judge/1.py
+++ pieces/文计助教/assignment5/judge/1.py
@@ -1,2 +1,48 @@
-with open('buf.txt','w') as f:
-    f.write('hello')
+from bs4 import BeautifulSoup
+import requests
+res = requests.get('http://news.sina.com.cn/hotnews/20181030.shtml')
+res.encoding = "utf-8"
+htmlCode = res.text
+oBS = BeautifulSoup(htmlCode,"html.parser")
+# print(oBS.prettify())
+tags = oBS.find_all(['th'])
+idx = 0
+d = {}
+for tag in tags:
+    if '媒体' in tag.get_text():
+        table_tag = tag.find_parent().find_parent()
+        tmp = table_tag.find_all('a')
+        if(len(tmp)):
+            d[idx] = tmp
+            idx += 1
+print(len(d))
+for k,v in d.items():
+    print()
+    print('-------------------------------------------------------')
+    print(k)
+    print(v)
+    print('-------------------------------------------------------')
+    print()
+
+# 图片排行
+tags = oBS.find_all(['h2'])
+idx = 0
+d = {}
+for tag in tags:
+    if '图片排行' in tag.get_text():
+        print(tag.find_parent().find_parent())
+
+
+
+# print(oBS.select('title'))
+# print(oBS.select("#4"))
+# view-source:http://news.sina.com.cn/hotnews/20181030.shtml#5
+# 井号之后是页面内定位用的，指向页面内的锚点（一个有名称的 a 标签）
+# <a name=5></a>
+
+
+
+# https://stackoverflow.com/questions/31958637/beautifulsoup-search-by-text-inside-a-tag
+
+# https://www.cnblogs.com/kongzhagen/p/6472746.html
+# https://blog.csdn.net/Winterto1990/article/details/47808949
--- pieces/文计助教/assignment5/judge/judge.py
+++ pieces/文计助教/assignment5/judge/judge.py
@@ -3,9 +3,99 @@ import zipfile
 
 root_path = '/Users/k/Downloads/resources/'
 
+zipfile_path = ''
+for file in os.listdir(root_path):
+    if file != '.DS_Store':
+        zipfile_path = os.path.join(root_path, file)
+        break
+
+unzip_path = os.path.join(root_path, "unzip")
+print(zipfile_path)  # 无法解压rar
+files = zipfile.ZipFile(zipfile_path, 'r')
+for file in files.namelist():
+    files.extract(file, unzip_path)
+files.close()
+
+# print(unzip_path)
+if '__MACOSX' in os.listdir(unzip_path):
+    if len(os.listdir(unzip_path)) == 2:
+        print('case 1')
+        unzip_good_path = ''
+        for file in os.listdir(unzip_path):
+            if file != '__MACOSX' and file != '.DS_Store':
+                unzip_good_path = os.path.join(unzip_path, file)
+                break
+
+        filename = ''
+        if unzip_good_path:
+            for file in os.listdir(unzip_good_path):
+                if '.py' in file:
+                    filename = file
+                    break
+        #     print(filename)
+        py_path = os.path.join(unzip_good_path, filename)
+    else:
+        print('case 2')
+        filename = ''
+        for file in os.listdir(unzip_path):
+            if '.py' in file:
+                filename = file
+                break
+        #   print(filename)
+        py_path = os.path.join(unzip_path, filename)
+elif len(os.listdir(unzip_path)) >= 3:
+    print('case 3')
+    filename = ''
+    for file in os.listdir(unzip_path):
+        if '.py' in file:
+            filename = file
+            break
+    py_path = os.path.join(unzip_path, filename)
+else:  # upzip_path/1800015422_20181017/xxx.txt
+    print('case 4')
+    unzip_good_path = ''
+    for file in os.listdir(unzip_path):
+        if file != '__MACOSX' and file != '.DS_Store':
+            unzip_good_path = os.path.join(unzip_path, file)
+            break
+    filename = ''
+    for file in os.listdir(unzip_good_path):
+        if '.py' in file:
+            filename = file
+            break
+    py_path = os.path.join(unzip_good_path, filename)
+
+print(os.path.split(py_path)[0])
+for file in os.listdir(os.path.split(py_path)[0]):
+    if '.txt' in file:
+        os.remove(os.path.join(os.path.split(py_path)[0], file))
+
+raw_path1 = '/Users/k/Documents/prac_code/pieces/文计助教/assignment5/judge/resources2/红楼梦.txt'
+raw_path2 = '/Users/k/Documents/prac_code/pieces/文计助教/assignment5/judge/resources2/水浒传.txt'
+from shutil import copyfile
+
+copyfile(raw_path1, os.path.split(py_path)[0] + '/' + '红楼梦.txt')
+copyfile(raw_path2, os.path.split(py_path)[0] + '/' + '水浒传.txt')
+
+# run
+os.chdir(os.path.split(py_path)[0])
+os.system('python ' + py_path)
+
+# import subprocess
+# os.chdir(os.path.join(raw_path, path_list[1]))
+# p = subprocess.Popen("python "+item,stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+# # p.terminate()
+# # p.kill()
+# #这个bug很难复现出来，似乎是因为直接kill或者terminate，就会导致文件没有保存。
+# p.wait() #必须要wait
+# # print(os.getcwd())
+# # print(os.listdir('.'))
+# # os.system("python " + item) # 用os.system 没有上面的问题，但是在windows上可能出问题,没有os.system的接口
+
+
 pre_path1 = '/Users/k/Documents/prac_code/pieces/文计助教/assignment5/judge/resources1/'
 txt_list1 = [pre_path1 + str(i) + '.txt' for i in range(2, 6)]
-pre_path2 = '/Users/k/Downloads/resources/upzip/1800015446/'
+pre_path2 = os.path.split(py_path)[0] + '/'
 txt_list2 = [pre_path2 + str(i) + '.txt' for i in range(2, 6)]
 
 flag = [True] * 4
@@ -23,11 +113,19 @@ def parse(paramPath):
     top500_dict = {}
     after500_dict = {}
     counter = 1
+    is_first = True
+    is_hundred = False
     for line in rFile.readlines():
         line = line.strip()
         line = re.split(",|\s+", line)
         try:
             line[1] = float(line[1])
+            if (is_first):
+                if (line[1] > 1):
+                    is_hundred = True
+                is_first = False
+            if (is_hundred):
+                line[1] /= 100
         except:
             print(paramPath)
             print(line)
@@ -44,7 +142,7 @@ def parse(paramPath):
 
 
 # 递减
-def judgeSeq(List):
+def decSeq(List):
     pre = 1
     for item in List:
         if pre < item[1]:
@@ -53,22 +151,26 @@ def judgeSeq(List):
     return True
 
 
-def parse2(paramPath):
+def get_list(paramPath):
     rFile = open(paramPath, "r", encoding="utf-8")
     result_list = []
     for line in rFile.readlines():
         line = line.strip()
         line = re.split(",|\s+", line)
-        result_list.append(line)
+        #         result_list.append(line)
+        result_list.extend(line)
     rFile.close()
     return result_list
 
 
-def parse3(paramPath):
+def get_L(paramPath):
     rFile = open(paramPath, "r", encoding="utf-8")
     rContent = rFile.read()
     rContent = rContent.strip()
-    result_list = re.split(",|\s+", rContent)
+    if (rContent[-1] == ','):
+        print('fuck!!!!!!! ,')
+        rContent = rContent[:-1]
+    result_list = re.split("(,|s)+", rContent)
     rFile.close()
     return result_list
 
@@ -78,6 +180,7 @@ def diff(L1, L2):
     len2 = len(L2)
     min_len = min(len1, len2)
     diff_list = []
+    i = 0
     for i in range(min_len):
         if L1[i] != L2[i]:
             diff_list.append((L1[i], L2[i]))
@@ -97,19 +200,14 @@ def diff(L1, L2):
 # 2
 list_11, list_12, dic11, dic12 = parse(txt_list1[0])
 list_21, list_22, dic21, dic22 = parse(txt_list2[0])
-if not (judgeSeq(list_21) and judgeSeq(list_22)):
+if not decSeq(list_21):
     print('here wrong1')
     flag[0] = False
-if not (dic11 == dic21 and dic12 == dic22):
+if not (dic11 == dic21):
     print('here wrong2')
     flag[0] = False
 
-# print(list_11)
-# print(list_12)
-print(diff(list_11, list_21))
-print(diff(list_12, list_22))
-# print(list_21)
-# print(list_22)
+diff(list_11, list_21)
 
 # 3
 rfile1 = open(txt_list1[1], "r", encoding="utf-8")
@@ -118,29 +216,48 @@ file1_content = rfile1.read()
 file2_content = rfile2.read()
 rfile1.close()
 rfile2.close()
-if not file1_content == file2_content:
+L1 = list(map(int, (re.findall(r'[0-9]+', file1_content))))
+L2 = list(map(int, (re.findall(r'[0-9]+', file2_content))))
+spaces = re.findall(r's+', file2_content)
+if spaces:
+    print('fuck!!!!, spaces')
+
+if not L1 == L2:
     flag[1] = False
+    print(diff(L1, L2))
 
 # 4
-li_list = [[], []]
-li_list[0] = parse2(txt_list1[2])
-li_list[1] = parse2(txt_list2[2])
+li_lists = []
+li_lists.append(get_list(txt_list1[2]))
+li_lists.append(get_list(txt_list2[2]))
 
-for i in range(2):
-    if not len(li_list[0][i]) == len(li_list[1][i]):
+# print(li_lists[0])
+# print(diff(li_lists[0],li_lists[1]))
+# for i in range(2):
+#     if not len(li_lists[0][i]) == len(li_lists[1][i]):
+#         flag[2] = False
+#     for cha in li_lists[1][i]:
+#         if not cha in li_lists[0][i]:
+#             flag[2] = False
+# 有的同学先乘了100 在舍入，导致有相同频率的值。。。
+if (set(li_lists[0]) != set(li_lists[1])):
+    minus = set(li_lists[0]) - set(li_lists[1])
+    if len(minus) >= 2:
         flag[2] = False
-    for cha in li_list[1][i]:
-        if not cha in li_list[0][i]:
-            flag[2] = False
+        print(len(minus))
+        print(minus)
 
 # 5
-li_list = []
-li_list.append(parse3(txt_list1[3]))
-li_list.append(parse3(txt_list2[3]))
+li_lists = []
+li_lists.append(get_L(txt_list1[3]))
+li_lists.append(get_L(txt_list2[3]))
 
-for cha in li_list[1]:
-    if not cha in li_list[0]:
+for ch in li_lists[1]:
+    if not ch in li_lists[0]:
+        print(ch)
         flag[3] = False
+if (not flag[3]):
+    diff(li_lists[0], li_lists[1])
 
 print(flag)
 
@@ -150,4 +267,12 @@ print(flag)
 #     os.remove(os.path.join(os.getcwd(),os.path.join(path_list[1], item)))
 
 
-
+if flag == [True] * 4:
+    import shutil
+    root_path = '/Users/k/Downloads/resources'
+    for file in os.listdir(root_path):
+        path = os.path.join(root_path,file)
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.remove(path)
