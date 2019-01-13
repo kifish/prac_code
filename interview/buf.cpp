d3274c19f6c5361c8f36e25c93a9f05377f9f225
kifish <huakaihuakai@qq.com>
2019-01-13 14:50:20 +0800

    -

--- interview/buf.cpp
+++ interview/buf.cpp
@@ -1,56 +1,42 @@
 #include <cstdio>
-#include <cstdlib>
-#include <string>
-#include <stack>
-#include <iostream>
+#include <queue>
 using namespace std;
-int main(){
-    stack<string> raw;
-    stack<double> nums;
-    string s;
-    while(cin>>s){
-        raw.push(s);
+struct X
+{
+    int v, q, j, n, f;
+    bool operator<(const X &a) const
+    {
+        if (q == a.q)
+            return j > a.j;
+        return q > a.q;
+    } //重载运算符
+} x[10005];
+priority_queue<X> qq; //调用系统堆
+int main()
+{
+    int k, n, m;
+    scanf("%d%d%d", &k, &n, &m);
+    for (int i = 1; i <= m; i++)
+    {
+        int u;
+        scanf("%d%d%d%d", &u, &x[i].v, &x[i].q, &x[i].j);
+        x[i].n = x[u].f;
+        x[u].f = i;
     }
-    while (!raw.empty()){
-        s = raw.top();
-        raw.pop();
-        if(s == "*"){
-            double b = nums.top();
-            nums.pop();
-            double a = nums.top();
-            nums.pop();
-            double res = a * b;
-            nums.push(res);
+    qq.push((X){1, 0, 0, 0, 0}); //载入初始节点
+    while (!qq.empty())
+    {
+        X t = qq.top();
+        qq.pop();
+        if (t.v == n)
+        {
+            printf("%d", t.q); //到达n即为最优解
+            return 0;
         }
-        else if(s == "/"){
-            double b = nums.top();
-            nums.pop();
-            double a = nums.top();
-            nums.pop();
-            double res = a / b;
-            nums.push(res);
-        }
-        else if(s == "+"){
-            double b = nums.top();
-            nums.pop();
-            double a = nums.top();
-            nums.pop();
-            double res = a + b;
-            nums.push(res);
-        }
-        else if(s == "-"){
-            double b = nums.top();
-            nums.pop();
-            double a = nums.top();
-            nums.pop();
-            double res = a - b;
-            nums.push(res);
-        }
-        else{
-            double num = atof(s.c_str());
-            nums.push(num);
-        }
+        for (int i = x[t.v].f; i; i = x[i].n)
+            if (t.j + x[i].j <= k)
+                qq.push((X){x[i].v, x[i].q + t.q, x[i].j + t.j, 0, 0});
     }
-    printf("%f\n",nums.top());
+    printf("-1");
     return 0;
 }
