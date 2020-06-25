75190858901bf1fbec1c7fda697ca9ace4dc2e53
kifish <huakaihuakai@qq.com>
2020-06-25 15:56:03 +0800

    save

--- algs4/pieces/HelloWorld.java
+++ algs4/pieces/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
-    public static void main(String[] args){
+        public static void main(String[] args){
         int x = 3;
         x = x;
         System.out.println("Hello,World");
