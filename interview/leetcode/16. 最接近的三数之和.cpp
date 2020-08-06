50dc0dd0da035cc80d258f313898f5b386b5e00a
kifish <huakaihuakai@qq.com>
2020-08-06 19:33:43 +0800

    update

---
+++ interview/leetcode/16. 最接近的三数之和.cpp
@@ -0,0 +1,12 @@
+/*
+
+https://leetcode-cn.com/problems/3sum-closest/solution/zui-jie-jin-de-san-shu-zhi-he-by-leetcode-solution/
+*/
+
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+
+    }
+};
---
+++ interview/leetcode/18. 四数之和.cpp
@@ -0,0 +1,11 @@
+/*
+https://leetcode-cn.com/problems/4sum/solution/shuang-zhi-zhen-jie-fa-can-zhao-san-shu-zhi-he-ge-/
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+    }
+};
