c550ce9217e0a0cf00c88de3487a1333ccdf840c
kifish <huakaihuakai@qq.com>
2020-08-12 01:02:30 +0800

    update

--- interview/leetcode/93. 复原IP地址.cpp
+++ interview/leetcode/93. 复原IP地址.cpp
@@ -1,12 +1,25 @@
+/*
 
-// https://leetcode-cn.com/problems/restore-ip-addresses/solution/fu-yuan-ipdi-zhi-by-leetcode/
+工程题
 
-// https://leetcode-cn.com/problems/restore-ip-addresses/
-// https://leetcode-cn.com/explore/interview/card/bytedance/242/string/1044/
+dfs + path 回溯
 
-// dfs + path 回溯
+https://leetcode-cn.com/problems/restore-ip-addresses/solution/fu-yuan-ipdi-zhi-by-leetcode/
 
-// 工程中也有可能用到
+https://leetcode-cn.com/problems/restore-ip-addresses/
+https://leetcode-cn.com/explore/interview/card/bytedance/242/string/1044/
+
+
+
+https://leetcode-cn.com/problems/restore-ip-addresses/solution/hui-su-suan-fa-hua-tu-fen-xi-jian-zhi-tiao-jian-by/
+
+
+c++实现:
+https://leetcode-cn.com/problems/restore-ip-addresses/solution/dfszi-fu-chuan-cao-zuo-93-fu-yuan-ipdi-zhi-by-zxre/
+
+
+*/
+
 
 class Solution {
 public:
---
+++ interview/leetcode/99. 恢复二叉搜索树.cpp
@@ -0,0 +1,17 @@
+/*
+
+https://leetcode-cn.com/problems/recover-binary-search-tree/
+
+
+用栈实现 中序遍历
+
+
+https://leetcode-cn.com/problems/recover-binary-search-tree/solution/hui-fu-er-cha-sou-suo-shu-by-leetcode-solution/
+
+
+莫里斯遍历
+
+https://leetcode-cn.com/problems/recover-binary-search-tree/solution/san-chong-jie-fa-xiang-xi-tu-jie-99-hui-fu-er-cha-/535568
+
+
+*/
