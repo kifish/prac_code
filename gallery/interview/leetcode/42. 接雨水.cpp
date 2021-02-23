a7fdb23eb1c876bff5afd894ecfbbd5c8b150359
kifish <huakaihuakai@qq.com>
2021-02-24 01:02:56 +0800

    update

--- gallery/interview/leetcode/42. 接雨水.cpp
+++ gallery/interview/leetcode/42. 接雨水.cpp
@@ -62,27 +62,27 @@ https://leetcode-cn.com/problems/trapping-rain-water/solution/jie-yu-shui-by-lee
 // 的用户
 
 // 双向DP
-class Solution {
-public:
-    int trap(vector<int>& height) {
-        if(height.size() == 0) return 0;
-        int sum = 0;
-        // vector<int> dp_left = vector<int> (height.size(), 0);
-        vector<int> dp_right = vector<int> (height.size(), 0);
-        int max_left = 0, max_right = 0;
-        for(int j = (int) height.size() - 1;j>=0;j--){
-            max_right = max(max_right,height[j]);
-            dp_right[j] = max_right;
-        }
-        max_left = height[0];
-        for(int i = 1;i < height.size(); i++){
-            max_left = max(max_left, height[i]);
-            max_right = dp_right[i];
-            sum += min(max_left,max_right) - height[i];
-        }
-        return sum;
-    }
-};
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         if(height.size() == 0) return 0;
+//         int sum = 0;
+//         // vector<int> dp_left = vector<int> (height.size(), 0);
+//         vector<int> dp_right = vector<int> (height.size(), 0);
+//         int max_left = 0, max_right = 0;
+//         for(int j = (int) height.size() - 1;j>=0;j--){
+//             max_right = max(max_right,height[j]);
+//             dp_right[j] = max_right;
+//         }
+//         max_left = height[0];
+//         for(int i = 1;i < height.size(); i++){
+//             max_left = max(max_left, height[i]);
+//             max_right = dp_right[i];
+//             sum += min(max_left,max_right) - height[i];
+//         }
+//         return sum;
+//     }
+// };
 
 
 
@@ -92,3 +92,51 @@ public:
 
 // 这个讲解的非常好
 
+
+// 单调栈: https://leetcode-cn.com/problems/trapping-rain-water/solution/dan-diao-zhan-jie-jue-jie-yu-shui-wen-ti-by-sweeti/
+
+// https://leetcode-cn.com/problems/trapping-rain-water/solution/dan-diao-zhan-jie-jue-jie-yu-shui-wen-ti-by-sweeti/327939
+
+/*
+
+执行结果：
+通过
+显示详情
+执行用时：
+4 ms
+, 在所有 C++ 提交中击败了
+97.14%
+的用户
+内存消耗：
+14.2 MB
+, 在所有 C++ 提交中击败了
+59.89%
+的用户
+
+*/
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if(height.size() == 0) return 0;
+        int sum = 0;
+        stack<int> S; // 维护一个单调递减的栈
+        for(int i = 0;i < height.size(); i++){
+            int the_right_baffle_h = height[i];
+            int the_right_baffle_idx = i;
+            // 将该区域作为挡板
+            while(!S.empty() && height[S.top()] < the_right_baffle_h){
+                int the_low_idx = S.top();
+                S.pop();
+                if(!S.empty()){
+                    int the_left_baffle_idx = S.top();
+                    int the_left_baffle_h = height[the_left_baffle_idx];
+                    sum += (min(the_left_baffle_h, the_right_baffle_h) - height[the_low_idx]) * (the_right_baffle_idx - the_left_baffle_idx - 1);
+                }
+            }
+            S.push(i); // 该区域作为洼地
+        }
+        return sum;
+    }
+};
