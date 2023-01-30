f6978e100316be8a5eab3687b0f3dc5cd9dbbd69
kifish <14976582+kifish@users.noreply.github.com>
2023-01-30 12:04:36 +0800

    Update 0026-删除排序数组中的重复项.cpp

--- gallery/interview/leetcode/0026-删除排序数组中的重复项.cpp
+++ gallery/interview/leetcode/0026-删除排序数组中的重复项.cpp
@@ -12,3 +12,22 @@ public:
         return idx+1;
     }
 };
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int left_idx = 1;
+        int right_idx = 1;
+        for(; right_idx < nums.size(); right_idx++){
+            if(nums[right_idx] == nums[right_idx - 1]){
+                continue;
+            }
+            else{
+                nums[left_idx++] = nums[right_idx];
+            }
+        }
+        return left_idx;
+    }
+};
