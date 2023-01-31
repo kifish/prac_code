9522d225c0e410a547812eb5269b131bfd918f81
kifish <14976582+kifish@users.noreply.github.com>
2023-01-31 20:50:17 +0800

    Update 395. 至少有K个重复字符的最长子串.cpp

--- gallery/interview/leetcode/395. 至少有K个重复字符的最长子串.cpp
+++ gallery/interview/leetcode/395. 至少有K个重复字符的最长子串.cpp
@@ -7,6 +7,6 @@ https://leetcode-cn.com/problems/longest-substring-with-at-least-k-repeating-cha
 
 https://leetcode-cn.com/problems/longest-substring-with-at-least-k-repeating-characters/solution/python-hua-dong-chuang-kou-by-summer_tre-fwa4/
 
-
-
+分治
+https://leetcode.cn/problems/longest-substring-with-at-least-k-repeating-characters/discussion/comments/1733118
 */
