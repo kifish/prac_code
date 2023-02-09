5ea1df20cb000f81d869f350892420ff298dd9eb
kifish <14976582+kifish@users.noreply.github.com>
2023-02-09 22:44:43 +0800

    Update binary_tree.py

--- gallery/base/datastruct/binary_tree.py
+++ gallery/base/datastruct/binary_tree.py
@@ -1,3 +1,15 @@
+"""
+二叉排序树，左子树小于等于根，右子树大于等于根。递归建立。
+中序遍历即可得不递减序列。
+
+前序遍历  根 左 右
+中序遍历  左 根 右
+后序遍历  左 右 根
+
+左子树永远先于右子树
+
+https://github.com/kifish/prac_code/blob/master/gallery/interview/leetcode/701.%20%E4%BA%8C%E5%8F%89%E6%90%9C%E7%B4%A2%E6%A0%91%E4%B8%AD%E7%9A%84%E6%8F%92%E5%85%A5%E6%93%8D%E4%BD%9C.md
+"""
 
 class Node(object):
     def __init__(self,val):
@@ -6,18 +18,6 @@ class Node(object):
         self.right = None 
 
 
-"""
-二叉排序树，左子树小于等于根，右子树大于等于根。递归建立。
-中序遍历即可得不递减序列。
-
-
-前序遍历  根 左 右
-中序遍历  左 根 右
-后序遍历  左 右 根
-
-左子树永远先于右子树
-
-"""
 class BinaryTree(object):
     def __init__(self,val):
         self.root = Node(val)
@@ -68,8 +68,6 @@ class BinaryTree(object):
             self.post_order(cur.right)
         print(cur.val,end = ' ')
 
-
-        
 
 if __name__ == '__main__':
     pass
