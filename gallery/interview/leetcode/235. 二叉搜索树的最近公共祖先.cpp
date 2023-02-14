3187f85d688d57909637db0c55d115b728a9f1f2
kifish <14976582+kifish@users.noreply.github.com>
2023-02-14 18:22:24 +0800

    Update 235. 二叉搜索树的最近公共祖先.cpp

--- gallery/interview/leetcode/235. 二叉搜索树的最近公共祖先.cpp
+++ gallery/interview/leetcode/235. 二叉搜索树的最近公共祖先.cpp
@@ -8,3 +8,29 @@ gallery\interview\leetcode\236. 二叉树的最近公共祖先.cpp
 
 
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr) return root;
+
+        if(root->val < p->val && root->val < q->val){
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        else if(root->val > p->val && root->val > q->val){
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        else{
+            return root;
+        }
+    }
+};
