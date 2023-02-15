e9470b3c4af9be763073f063b857da386449a107
kifish <14976582+kifish@users.noreply.github.com>
2023-02-15 19:31:28 +0800

    Update 134. 加油站.cpp

--- gallery/interview/leetcode/134. 加油站.cpp
+++ gallery/interview/leetcode/134. 加油站.cpp
@@ -1,14 +1,30 @@
 /*
-
 https://leetcode-cn.com/problems/gas-station/
-
-
 
 https://leetcode-cn.com/problems/gas-station/solution/jia-you-zhan-by-leetcode/
 
-
-分开来理解total_tank 和 curr_tank
-
-
-
+分开来理解total_sum和cur_sum
 */
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int start_idx = 0;
+        int cur_sum = 0;
+        int total_sum = 0;
+        for(int i = 0; i < gas.size(); i++){
+            cur_sum += gas[i] - cost[i];
+            total_sum += gas[i] - cost[i];
+            if(cur_sum < 0){
+                start_idx = i + 1;
+                cur_sum = 0;
+            }
+        }
+        if(total_sum < 0){
+            return -1;
+        }
+        else{
+            return start_idx;
+        }
+    }
+};
